bc1b412203230d3bcb7e1df19dd15064d0ad4873
Fred Chu <freddychu222@gmail.com>
2021-01-16 17:52:10 -0800

    6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 //ready data
 let time0 = 1200;
-let msg0 = "Hey there";
+let msg0 = "Hello there";
 function Ready() {
   time0 = document.getElementById('timebox').value;
   msg0 = document.getElementById('msgbox').value;
