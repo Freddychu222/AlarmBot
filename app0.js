dbb6483745e88c7e2ed192499993e140af8d9943
Fred Chu <freddychu222@gmail.com>
2021-01-16 18:34:25 -0800

    multiple send queires

--- app0.js
+++ app0.js
@@ -3,14 +3,21 @@ let time0 = 1200;
 let msg0 = "Hello there";
 function Ready() {
   time0 = document.getElementById('timebox').value;
+}
+
+function Ready2() {
   msg0 = document.getElementById('msgbox').value;
 }
 //backgroundb
 function backcall(){
   Ready();
-  firebase.database().ref('time/'+time0).set({
+  firebase.database().ref('Time').set({
     Time : time0,
+  });
+}
+function backcall2(){
+  Ready2();
+  firebase.database().ref('Message').set({
     Message : msg0
   });
-
 }
