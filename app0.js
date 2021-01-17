b81c3d9447863377c40f66b922fff9a530c464cd
Fred Chu <freddychu222@gmail.com>
2021-01-16 18:16:45 -0800

    new

---
+++ app0.js
@@ -0,0 +1,16 @@
+//ready data
+let time0 = 1200;
+let msg0 = "Hello there";
+function Ready() {
+  time0 = document.getElementById('timebox').value;
+  msg0 = document.getElementById('msgbox').value;
+}
+//backgroundb
+function backcall(){
+  Ready();
+  firebase.database().ref('time/'+time0).set({
+    Time : time0,
+    Message : msg0
+  });
+
+}
